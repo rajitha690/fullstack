d9d913e8091fbfd884a33f0f3c579e4e9f811ec0
dimpleswapna <swapnasowju2011@gmail.com>
2025-04-18 02:16:27 -0700

    Update taskServices.js

--- frontendreactjs/src/services/taskServices.js
+++ frontendreactjs/src/services/taskServices.js
@@ -1,18 +1,19 @@
 import axios from "axios";
+
 const apiUrl = process.env.REACT_APP_BACKEND_URL;
-console.log(apiUrl)
+
 export function getTasks() {
-    return axios.get(apiUrl);
+  return axios.get(apiUrl + "/courses");
 }
 
 export function addTask(task) {
-    return axios.post(apiUrl, task);
+  return axios.post(apiUrl + "/courses", task);
 }
 
 export function updateTask(id, task) {
-    return axios.put(apiUrl + "/" + id, task);
+  return axios.put(apiUrl + "/courses/" + id, task);
 }
 
 export function deleteTask(id) {
-    return axios.delete(apiUrl + "/" + id);
+  return axios.delete(apiUrl + "/courses/" + id);
 }

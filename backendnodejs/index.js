f98760965f5016c02585f0921e9a74d4ab98eb26
dimpleswapna <swapnasowju2011@gmail.com>
2025-04-19 08:18:22 -0700

    Update index.js

--- backendnodejs/index.js
+++ backendnodejs/index.js
@@ -20,12 +20,15 @@ app.options('*', cors());
 
 app.use(express.json());
 
+// Health Check Endpoint
 app.get('/health', (req, res) => {
   res.status(200).send('Backend is healthy');
 });
 
+// Course Routes
 app.use("/courses", courses);
 
+// Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Our backend API is listening via port ${port}`);
